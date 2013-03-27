9a51a267f66daf7ab20983500b50a635f933337e
curdog <curdog@gmail.com>
2013-03-27 18:35:58 -0400

    More setup

---
+++ db.hpp
@@ -0,0 +1,11 @@
+#ifndef _DB_HW3
+#define _DB_HW3
+
+class Db{
+public:
+
+private:
+
+};
+
+#endif
---
+++ person.hpp
@@ -0,0 +1,13 @@
+#ifndef _DB_PERSONHW3
+#define _DB_PERSONHW3
+
+class Person{
+public:
+
+private:
+  int ID;
+  char[25] name;
+  short int age;
+};
+
+#endif
